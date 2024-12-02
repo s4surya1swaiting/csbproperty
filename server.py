8ca4ebf2380a206d6418c776e63560889699b8cc
s4surya1swaiting <52548399+s4surya1swaiting@users.noreply.github.com>
2024-12-02 14:22:55 +0530

    Update server.py

--- server.py
+++ server.py
@@ -34,6 +34,4 @@ def predict_home_price():
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
+    app.run()
